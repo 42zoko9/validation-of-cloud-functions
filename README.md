b0e728a02d74a488433792f2d0d17175e5cdef32
42zoko9 <hexproof1988@gmail.com>
2021-11-09 18:20:24 +0900

    Add: init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # validation-of-cloud-functions
-cloud functionosの検証
+cloud functionosの検証  
+
+## Usage
+デプロイ用のコマンド  
+```
+gcloud functions deploy デプロイ名 \
+    --entry-point 実行するmain.pyの関数 \
+    --region asia-northeast1 \
+    --runtime python37 \
+    --memory 128MB \
+    --trigger-resource 任意のトピック \
+    --trigger-event google.pubsub.topic.publish \
+    --service-account cloudfunctionsとsecretmanagerのaccess権限を持ったサービスアカウント \
+    --set-env-vars SECRET_ID=呼び出すシークレット名
+```
+
+## Reference
+- [Secret Manager に保存した機密情報を、Cloud Functions の Python コードから取得してみた。](https://dev.classmethod.jp/articles/secret-manager-access-from-cloudfunctions-python/)
---
+++ main.py
@@ -0,0 +1,22 @@
+from google.cloud import secretmanager
+import os
+
+def access_secret_version(project_id, secret_id, version_id='latest'):
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
+    response = client.access_secret_version(request={"name": name})
+
+    # Print the secret payload.
+    payload = response.payload.data.decode("UTF-8")
+    print("Plaintext: {}".format(payload))
+
+def get_credential(event, context):
+    GCP_PROJECT = os.getenv('GCP_PROJECT')
+    SECRET_ID = os.getenv('SECRET_ID')
+
+    access_secret_version(GCP_PROJECT, SECRET_ID)
