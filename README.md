4894fa93dc2599e2175b72ab5c99b9e5cab7ccc4
42zoko9 <hexproof1988@gmail.com>
2021-11-20 15:48:44 +0900

    Update: secretの値を更新する処理のテスト

--- README.md
+++ README.md
@@ -18,4 +18,7 @@ gcloud functions deploy デプロイ名 \
 ```
 
 ## Reference
+- [Python Client for Secret Manager API](https://googleapis.dev/python/secretmanager/latest/index.html)
+- [Python Client for Secret Manager API (Beta)](https://googleapis.dev/python/secretmanager/0.1.1/index.html)
 - [Secret Manager に保存した機密情報を、Cloud Functions の Python コードから取得してみた。](https://dev.classmethod.jp/articles/secret-manager-access-from-cloudfunctions-python/)
+- [Secret Managerの使い方](https://mahito.hatenablog.com/entry/2020/06/16/093140)
--- main.py
+++ main.py
@@ -7,14 +7,23 @@ def access_secret_version(project_id, secret_id, version_id='latest'):
     client = secretmanager.SecretManagerServiceClient()
 
     # Build the resource name of the secret version.
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    parent_name = client.secret_version_path(project_id, secret_id)
+    name = client.secret_version_path(project_id, secret_id, version_id)
 
     # Access the secret version.
-    response = client.access_secret_version(request={"name": name})
+    response = client.access_secret_version(name)
 
     # Print the secret payload.
     payload = response.payload.data.decode("UTF-8")
     print("Plaintext: {}".format(payload))
+
+    # Update the secret payload
+    new_payload = str(int(payload) + 1)
+    response = client.destroy_secret_version(name)
+    response = client.add_secret_version(parent_name, {'data': new_payload.encode('utf-8')})
+    
+    # Print the new secret version name.
+    print('Added secret version: {}'.format(response.name))
 
 def get_credential(event, context):
     GCP_PROJECT = os.getenv('GCP_PROJECT')
